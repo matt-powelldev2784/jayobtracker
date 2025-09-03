206a58b27dcb4125f9593e038b54de02a1969a3c
Matthew Powell <matt.powell2784@gmail.com>
2025-09-03 20:32:52 +0100

    feat: add border to error card

--- components/ui/errorCard.tsx
+++ components/ui/errorCard.tsx
@@ -8,18 +8,18 @@ type ErrorCardProps = {
 
 const ErrorCard = ({ message }: ErrorCardProps) => {
   return (
-    <Card className="w-11/12 max-w-[700px] mt-8 pb-8">
-      <CardHeader className="bg-primary">
+    <Card className="w-11/12 max-w-[700px] mt-8 pb-8 border-2 border-2-secondary rounded-2xl">
+      <CardHeader className="bg-primary p-4">
         <TriangleAlert className="h-16 w-16 text-white" />
         <CardTitle className="text-center text-white">Error</CardTitle>
       </CardHeader>
 
       <CardDescription className="flex flex-col items-center gap-4 text-center">
         <div>
-          <CardTitle className="text-center text-black font-normal">
+          <CardTitle className="text-center text-black font-normal px-2">
             The application has encountered an Error. Please try again later.
           </CardTitle>
-          <p className="text-secondary">Error Message: {message}</p>
+          <p className="text-secondary px-2">Error Message: {message}</p>
         </div>
 
         <LinkButton href="/" className="text-white visited:text-white mt-4" variant="default">
