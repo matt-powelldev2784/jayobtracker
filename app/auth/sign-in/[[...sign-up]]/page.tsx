ae84eb286693b3061e2eaf473b1952f6a09fabe2
Matthew Powell <matt.powell2784@gmail.com>
2025-09-04 21:40:25 +0100

    feat: update sign in redirect url

--- app/auth/sign-in/[[...sign-up]]/page.tsx
+++ app/auth/sign-in/[[...sign-up]]/page.tsx
@@ -2,8 +2,8 @@ import { SignIn } from '@clerk/nextjs'
 
 const SignInPage = () => (
   <div className="pt-8 pb-24">
-    <SignIn />
+    <SignIn forceRedirectUrl="/view-jobs" />
   </div>
-)
+);
 
 export default SignInPage
